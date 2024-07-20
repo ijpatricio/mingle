50e900aa5c93010eb09d850911f1f00dbb712ca5
Joao Patricio <ijpatricio@gmail.com>
2024-07-20 13:39:22 +0100

    wip

--- wrapper-mingle/vite.config.js
+++ wrapper-mingle/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 import findMingles from './vendor/ijpatricio/mingle/resources/js/autoImport.js'
 const mingles = findMingles('resources/js')
-
+// Optional: Output the mingles to the console, for a visual check
 console.log('Auto-importing mingles:', mingles)
 
 export default defineConfig({
