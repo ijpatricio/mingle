d443ccb570ff53fb2f095d6c2e411cba9ef22857
Joao Patricio <ijpatricio@gmail.com>
2024-08-10 20:40:12 +0100

    Fix seeder

--- wrapper-mingle/database/seeders/DatabaseSeeder.php
+++ wrapper-mingle/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Demo Mingle',
-            'email' => 'demo@minglejs.unitedbycode.com',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
