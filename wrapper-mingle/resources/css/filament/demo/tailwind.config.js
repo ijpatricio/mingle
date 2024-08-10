02118f1215dc5db33fd8d2e94251c0c4e65d74b2
Joao Patricio <ijpatricio@gmail.com>
2024-08-10 14:56:19 +0100

    Cleanup

--- wrapper-mingle/resources/css/filament/demo/tailwind.config.js
+++ wrapper-mingle/resources/css/filament/demo/tailwind.config.js
@@ -3,9 +3,11 @@ import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
 export default {
     presets: [preset],
     content: [
+        './app/**/*.php',
+        './resources/views/**/*.{php, js, jsx, ts, tsx, vue, blade.php}',
+        './vendor/filament/**/*.blade.php',
+        './storage/framework/views/*.php',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.{php, js, jsx, ts, tsx, vue}',
     ],
 }
