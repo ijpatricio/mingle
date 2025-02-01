66d6b98a2fb6ad860a212f14393623a63c8401b4
Patricio <ijpatricio@gmail.com>
2025-02-01 11:26:35 +0000

    wip

---
+++ wrapper-mingle/app/Console/Commands/TestBladeCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Blade;
+
+class TestBladeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-blade';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info(
+            Blade::render('@mingles')
+        );
+    }
+}
