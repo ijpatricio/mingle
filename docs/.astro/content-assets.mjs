e97432435fcfe9907a0c1243985bd72adf94bd48
Joao Patricio <ijpatricio@gmail.com>
2025-03-01 17:03:41 +0000

    deploy docs action

---
+++ docs/.astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_Z2gGaEW from "../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z2gGaEW]]);
+		
