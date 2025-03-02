1d8005cbf1664b61a464db90235f90644fdbe19c
Joao Patricio <ijpatricio@gmail.com>
2025-03-02 19:19:00 +0000

    Views

--- playground/resources/views/layouts/app.blade.php
+++ playground/resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
-        @vite(['resources/js/Kanban/index.js', 'resources/css/app.css'])
+        @vite(['resources/css/app.css'])
     </head>
     <body>
     @yield('content')
--- playground/resources/views/welcome.blade.php
+++ playground/resources/views/welcome.blade.php
@@ -2,34 +2,26 @@
 
 @section('content')
     <div
-        class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
+        class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
+    >
         <div
-            class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-            <main class="flex w-full max-w-[335px] flex-col-reverse  lg:flex-row">
-                <div
-                    class="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
-                    <h1 class="mb-1 font-medium">Let's get started</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
-                        Laravel has an incredibly rich ecosystem.
-                        <br/>
-                        We suggest starting with the following.
-                    </p>
-
-                    <ul class="flex gap-3 text-sm leading-normal">
-                        <li>
-                            <a
-                                href="https://cloud.laravel.com"
-                                target="_blank"
-                                class="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white"
-                            >
-                                Deploy now
-                            </a>
-                        </li>
-                    </ul>
-
-                    <livewire:kanban />
-
-                </div>
+            class="flex flex-col gap-4 w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
+        >
+            <h1 class="mb-1 dark:text-white font-medium text-3xl">
+                Welcome to
+                <span
+                    class="title-gradient-text font-black text-transparent
+                            bg-clip-text bg-gradient-to-b
+                            from-indigo-700 to-pink-400
+                            dark:from-indigo-500 dark:to-pink-500"
+                >
+                          Mingle JS
+                        </span>
+            </h1>
+            <main class="flex flex-col gap-4 w-full max-w-2xl">
+                @foreach($showcases as $showcase)
+                    <x-showcase :$showcase />
+                @endforeach
             </main>
         </div>
     </div>
